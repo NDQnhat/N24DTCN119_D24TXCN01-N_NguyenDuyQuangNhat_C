46997c59f67598d6e20be1efa567b7808ca5c619
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-09 10:07:56 +0700

    upload Ex1_C_Session1

---
+++ Ex1_C_Session1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int a,b;
+	printf("nhap a: ");
+	scanf("%d",&a) ;
+	printf("nhap b: ");
+	scanf("%d",&b); 
+	if (a>b) {
+		printf("b nho hon"); 
+	} else { 
+	printf ("a nho hon"); 
+	} return 0;  
+} 
