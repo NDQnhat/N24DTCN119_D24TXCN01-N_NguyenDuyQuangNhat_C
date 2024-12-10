b1a7d6d7c62e8231a0aa50875cb774b67fe88cf7
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-10 19:24:56 +0700

    up Ex1_C_Session2

---
+++ Ex1_C_Session2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int integer; //kieu so nguyen 
+    float real=1.1111; //kieu so thuc
+    double Real; //kieu so thuc nhung rong hon
+    char character='S'; //kieu ky tu
+}
