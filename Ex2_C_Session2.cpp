e6c47689bf9472bb1d6eda35cc62a9fc7a70c0db
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-10 19:25:34 +0700

    upEx2_C_Session2

---
+++ Ex2_C_Session2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int x,y;
+	printf("nhap x:");
+	scanf("%d", &x);
+	printf("nhap y:");
+	scanf("%d", &y);
+	int sum=x+y;
+	printf("tong cua hai so la: %d",sum);
+	return 0;
+}
