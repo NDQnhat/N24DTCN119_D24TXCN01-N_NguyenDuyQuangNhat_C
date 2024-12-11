02dd9d0af55d965528f116238570fee2ffe0bd1d
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-11 15:18:48 +0700

    Update Ex7_C_Session3.cpp

--- Ex7_C_Session3.cpp
+++ Ex7_C_Session3.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
-	int num, sum;
+	int num;
 	printf("nhap so nguyen co 4 chu so: ");
 	scanf("%d", &num);
-	sum=(num/1000)+((num/100)%10)+((num/10)%10)+(num%10);
-	printf("tong cac chu so da nhap la: %d", sum);
+	printf("tong cac chu so da nhap la: %d", (num/1000)+((num/100)%10)+((num/10)%10)+(num%10));
 	return 0;
 }
